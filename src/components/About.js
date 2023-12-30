beb9b48034e6ba0b245d8f16fc73429420cc1a9b
ajay sonere <ajaysonere786@gmail.com>
2023-12-31 00:20:37 +0530

    about added

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,33 @@
-import aboutBanner from '../assests/About Page.png';
-import aboutImage from '../assests/Homepage.png'
+import aboutBanner from '../assests/About Section Background.png';
+import aboutImage from '../assests/About Page.png';
 
 function About(){
-    return(
-       <section>
-          <div className="container about-container">
-              <img src={aboutBanner} alt='about'></img>
+    return (
+      <section>
+        <div className="container about-container">
+          <img src={aboutImage} alt="about"></img>
+          <div className="about-left">
+            <small className='section-heading'>About</small>
+            <h2>Food Is An Important <br /> Part Of A Balanced <br/> Diet</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et
+              elit. Dolor turpis molestie dui magnis facilisis at fringilla
+              quam.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et
+              elit. Dolor turpis molestie dui magnis facilisis at fringilla
+              quam.
+            </p>
+            <div className="about-icons">
+              <button className="book-now hero-btn"> Learn More</button>
+              <span class="material-symbols-outlined">play_circle</span>
+              <small>Watch video</small>
+            </div>
           </div>
-          <img src={aboutBanner} alt='about' className='about-banner'></img>
-       </section>
+        </div>
+        <img src={aboutBanner} alt="about" className="about-banner"></img>
+      </section>
     );
 };
 
